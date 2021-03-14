9b85d7ff5eab760dbfd347206ce974ce57deb1f1
Chris Anders <filmee24@gmail.com>
2021-03-14 18:53:14 +0100

    Add parsing EntityModel without Property Parsing

--- Source/GrandTextAdventure/GrandTextAdventure.Core/Parsers/EntityParser/EntityDefinitionParser.cs
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/Parsers/EntityParser/EntityDefinitionParser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using GrandTextAdventure.Core.Parsers.EntityParser.Syntax;
 using GrandTextAdventure.Core.Parsing;
+using GrandTextAdventure.Core.Parsing.Tokenizer;
 
 namespace GrandTextAdventure.Core.Parsers.EntityParser
 {
@@ -23,8 +24,8 @@ namespace GrandTextAdventure.Core.Parsers.EntityParser
             Tokenizer.AddDefinition(SyntaxKind.IsToken, "is", 1);
             Tokenizer.AddDefinition(SyntaxKind.ApplyModelToken, "appylmodel", 1);
 
-            Tokenizer.AddDefinition(SyntaxKind.IntLiteralToken, "[0-9]+", 1, typeof(int));
-            Tokenizer.AddDefinition(SyntaxKind.StringLiteralToken, @"'.*?'", 1);
+            Tokenizer.AddDefinition(SyntaxKind.IntLiteralToken, "[0-9]+", 2, typeof(int));
+            Tokenizer.AddDefinition(SyntaxKind.StringLiteralToken, "\".*?\"", 2);
             Tokenizer.AddDefinition(SyntaxKind.CommentToken, @"/\\*.*?\\*/", 1);
             Tokenizer.AddDefinition(SyntaxKind.IdentifierToken, "[a-zA-Z_][0-9a-zA-F_]*", 1);
 
@@ -35,9 +36,16 @@ namespace GrandTextAdventure.Core.Parsers.EntityParser
         {
             var block = ParseBlock();
 
-            MatchToken(SyntaxKind.EOF);
+            MatchToken(SyntaxKind.EndOfFile);
 
             return block;
+        }
+
+        private static Token RemoveStringCharakter(Token token)
+        {
+            var content = token.Text.Substring(1, token.Length - 2);
+
+            return new Token(token.Kind, content, token.Start, token.End, token.ReturnType);
         }
 
         private SyntaxNode ParseEntityDefinition()
@@ -47,33 +55,43 @@ namespace GrandTextAdventure.Core.Parsers.EntityParser
 
         private SyntaxNode ParseEntityModelDefinition()
         {
-            throw new NotImplementedException();
+            var keywordToken = NextToken();
+            var nameToken = MatchToken(SyntaxKind.StringLiteralToken);
+
+            nameToken = RemoveStringCharakter(nameToken);
+
+            var properties = new BlockNode(null);
+
+            return new EntityModelDefinitionNode(keywordToken, nameToken, properties);
         }
 
         private SyntaxNode ParseMember()
         {
-            //ToDo: refactor ParseMember to a Dictionary to reduce branches
+            SyntaxNode node;
             if (Current.Kind == SyntaxKind.EntityModelToken)
             {
-                return ParseEntityModelDefinition();
+                node = ParseEntityModelDefinition();
             }
-            if (Current.Kind == SyntaxKind.EntityToken)
+            else if (Current.Kind == SyntaxKind.EntityToken)
             {
-                return ParseEntityDefinition();
+                node = ParseEntityDefinition();
             }
             else
             {
                 Diagnostics.ReportUnexpectedDeclaration(Current.Span, Current);
+                return null;
             }
 
-            return null;
+            MatchToken(SyntaxKind.EndToken);
+
+            return node;
         }
 
         private IEnumerable<SyntaxNode> ParseMembers()
         {
             var members = new List<SyntaxNode>();
 
-            while (Current.Kind != SyntaxKind.EOF)
+            while (Current.Kind != SyntaxKind.EndOfFile)
             {
                 var startToken = Current;
                 var member = ParseMember();
@@ -84,7 +102,10 @@ namespace GrandTextAdventure.Core.Parsers.EntityParser
                 }
                 else
                 {
-                    members.Add(member);
+                    if (member != null)
+                    {
+                        members.Add(member);
+                    }
                 }
 
                 // If ParseMember() did not consume any tokens,
