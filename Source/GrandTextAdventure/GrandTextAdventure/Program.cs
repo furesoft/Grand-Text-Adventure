8323b6318f84fa15f3591b222f98a4c7529122a5
Chris Anders <filmee24@gmail.com>
2021-03-14 12:36:18 +0100

    Cleanup

--- Source/GrandTextAdventure/GrandTextAdventure/Program.cs
+++ Source/GrandTextAdventure/GrandTextAdventure/Program.cs
@@ -4,7 +4,7 @@ namespace GrandTextAdventure
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             Console.WriteLine("Hello World!");
         }
