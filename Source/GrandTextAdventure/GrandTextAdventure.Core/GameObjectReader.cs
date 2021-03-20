c456479d9570f944aff987bc3763e9be38dd073f
Chris Anders <filmee24@gmail.com>
2021-03-20 15:15:48 +0100

    Started GameObjectReader

---
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/GameObjectReader.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using LibObjectFile.Elf;
+
+namespace GrandTextAdventure.Core
+{
+    public class GameObjectReader
+    {
+        private readonly Stream _strm;
+        private ElfObjectFile _file;
+
+        public GameObjectReader(Stream strm)
+        {
+            _file = ElfObjectFile.Read(strm);
+            _strm = strm;
+        }
+
+        public void Close()
+        {
+            _strm.Close();
+        }
+    }
+}
---
+++ Source/UnitTests/ObjectReaderTests.cs
@@ -0,0 +1,18 @@
+﻿using GrandTextAdventure.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ObjectReaderTests
+    {
+        [TestMethod]
+        public void Read_Should_Pass()
+        {
+            var reader = new GameObjectReader(File.OpenRead("test.ced"));
+
+            reader.Close();
+        }
+    }
+}
