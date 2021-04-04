1454bc7cba5da8693f4a4f8b0cbcb6e178702f6e
Chris Anders <filmee24@gmail.com>
2021-04-04 16:45:40 +0200

    Add WhoAmICommand

---
+++ Source/GrandTextAdventure/GrandTextAdventure/Commands/WhoAmICommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using GrandTextAdventure.Core.CommandProcessing;
+using GrandTextAdventure.Messages;
+
+namespace GrandTextAdventure.Commands
+{
+    [CommandPattern("Who am I?")]
+    public class WhoAmICommand : ICommand
+    {
+        public void Invoke(Match args)
+        {
+            var msg = (GetStateMessage)Program.Mailbox.PostAndReply<GameMessage>((channel) => new GetStateMessage(channel, "/player/Name"));
+
+            System.Console.WriteLine("You are " + msg.Value);
+        }
+    }
+}
