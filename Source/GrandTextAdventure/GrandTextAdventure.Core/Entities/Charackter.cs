796d382abc966e0bc105cc4d44d790b6813d092b
Chris Anders <filmee24@gmail.com>
2021-04-04 16:44:30 +0200

    Add Name override of Player

--- Source/GrandTextAdventure/GrandTextAdventure.Core/Entities/Charackter.cs
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/Entities/Charackter.cs
@@ -5,6 +5,9 @@
         public Charackter()
         {
             Type = GameObjectType.Charackter;
+            Name = "Michael";
         }
+
+        public override string Name { get => GetValue<string>(nameof(Name)); set => SetOrAddValue(nameof(Name), value); }
     }
 }
