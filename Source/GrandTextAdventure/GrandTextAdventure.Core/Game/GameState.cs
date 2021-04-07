321a48fb011fd34b981897921fa8841cb74f83e5
Chris Anders <filmee24@gmail.com>
2021-04-07 13:24:20 +0200

    Add initial Room exits

--- Source/GrandTextAdventure/GrandTextAdventure.Core/Game/GameState.cs
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/Game/GameState.cs
@@ -8,6 +8,10 @@ namespace GrandTextAdventure.Core.Game
         {
             Player.SetOrAddValue("Money", new Money { PilzschafCoins = 1, Dollar = 500 });
             CurrentMap = new Room { Name = "Baker Street" };
+            CurrentMap.Exits.North = new Room { Name = "Suffer Street", Exits = new RoomExits { South = CurrentMap } };
+            CurrentMap.Exits.South = new Room { Name = "London Street", Exits = new RoomExits { North = CurrentMap } };
+            CurrentMap.Exits.East = new Room { Name = "Hospital Street", Exits = new RoomExits { West = CurrentMap } };
+            CurrentMap.Exits.West = new Room { Name = "Ball Street", Exits = new RoomExits { East = CurrentMap } };
         }
 
         public Room CurrentMap { get; set; }
