a4f8c540074c3faab55b0df1e0bdf92bed154988
Chris <filmee24@gmail.com>
2021-04-16 14:28:07 +0200

    Cleanup

--- Source/GrandTextAdventure/GrandTextAdventure/Commands/HelpCommand.cs
+++ Source/GrandTextAdventure/GrandTextAdventure/Commands/HelpCommand.cs
@@ -4,6 +4,7 @@ using GrandTextAdventure.Core.CommandProcessing;
 namespace GrandTextAdventure.Commands
 {
     [CommandPattern(@"What can I say\?")]
+    [CommandPattern(@"help")]
     internal class HelpCommand : ICommand
     {
         public void Invoke(Match args)
