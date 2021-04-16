5b5a1d0c188af31e82d9dc979d740b3b852bad2c
Chris <filmee24@gmail.com>
2021-04-16 15:24:48 +0200

    Cleanup

--- Source/GrandTextAdventure/GrandTextAdventure/Program.cs
+++ Source/GrandTextAdventure/GrandTextAdventure/Program.cs
@@ -8,10 +8,12 @@ namespace GrandTextAdventure
     {
         private static void Main()
         {
+            // Use FigLet font for Banner
             var fontStream = Assembly.GetEntryAssembly().GetManifestResourceStream("GrandTextAdventure.font.flf");
             var font = new WenceyWang.FIGlet.FIGletFont(fontStream);
             var text = new WenceyWang.FIGlet.AsciiArt("Grand Text Adventure", font: font, width: WenceyWang.FIGlet.CharacterWidth.Full);
             Console.WriteLine(text.ToString());
+            Console.WriteLine();
 
             EntitiyPreloader.PreLoad(Environment.CurrentDirectory);
 
