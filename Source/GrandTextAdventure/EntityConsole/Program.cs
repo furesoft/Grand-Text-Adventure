273033120c2a00791e243d1a1f460602c5d1d362
Chris <filmee24@gmail.com>
2021-04-18 15:04:33 +0200

    Add Core for EnttiyConsole
    to build and edit efl files

---
+++ Source/GrandTextAdventure/EntityConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace EntityConsole
+{
+    class Program
+    {
+        // load-file
+        // create-file
+        // save file
+
+        // add-object (vehicle|npc|weapon)
+        // delete-object
+        // add-property
+        // delete-property
+        // change-property
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = ReadLine.Read("> ");
+
+                ReadLine.AddHistory(input);
+
+                var cmd = Command.FromString(input);
+
+            }
+        }
+    }
+}
