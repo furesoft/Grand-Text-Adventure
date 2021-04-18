e20b23560fadd71481efc4076c15e25416ffda85
Chris <filmee24@gmail.com>
2021-04-18 08:17:41 +0200

    Add CommandHandler for NoCommand Verb

---
+++ Source/GrandTextAdventure/GrandTextAdventure/Commands/NoCommandHandler.cs
@@ -0,0 +1,15 @@
+using GrandTextAdventure.Core.TextProcessing;
+using GrandTextAdventure.Core.TextProcessing.Interfaces;
+using GrandTextAdventure.Core.TextProcessing.Synonyms;
+
+namespace GrandTextAdventure.Commands
+{
+    [CommandHandler(VerbCodes.NoCommand)]
+    public class NoCommandHandler : ICommandHandler
+    {
+        public void Invoke(Command cmd)
+        {
+            System.Console.WriteLine("I can't understand what you mean");
+        }
+    }
+}
