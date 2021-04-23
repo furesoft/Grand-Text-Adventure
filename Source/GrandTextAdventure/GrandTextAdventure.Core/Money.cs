ef3a2d0cf04d4a9c16727fb827fac8d0c0dac8d5
Chris <filmee24@gmail.com>
2021-04-23 18:47:39 +0200

    Add Operator Overload For Money Type

--- Source/GrandTextAdventure/GrandTextAdventure.Core/Money.cs
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/Money.cs
@@ -1,8 +1,30 @@
-﻿namespace GrandTextAdventure.Core
+﻿using System.Runtime.ExceptionServices;
+
+namespace GrandTextAdventure.Core
 {
     public class Money
     {
         public long Dollar { get; set; }
         public int PilzschafCoins { get; set; }
+
+        public static Money operator +(Money first, Money second)
+        {
+
+            return new Money
+            {
+                Dollar = first.Dollar + second.Dollar,
+                PilzschafCoins = first.PilzschafCoins + second.PilzschafCoins
+            };
+        }
+
+        public static Money operator -(Money first, Money second)
+        {
+
+            return new Money
+            {
+                Dollar = first.Dollar - second.Dollar,
+                PilzschafCoins = first.PilzschafCoins - second.PilzschafCoins
+            };
+        }
     }
 }
