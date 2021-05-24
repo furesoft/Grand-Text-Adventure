53a64cd0ff59c423ef490b039b97961c66049665
Chris <filmee24@gmail.com>
2021-05-24 09:36:18 +0200

    Cleanup

--- Source/GrandTextAdventure/GrandTextAdventure.Core/Entities/PlayerCharackter.cs
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/Entities/PlayerCharackter.cs
@@ -11,9 +11,9 @@ namespace GrandTextAdventure.Core.Entities
 
         private void OnDead_Handler(uint health)
         {
-            Console.WriteLine("You are dead. You will spawn near a hospital");
+            GameEngine.Instance.Wait(1500);
 
-            
+            Console.WriteLine("You are dead. You will spawn near a hospital");
 
             //ToDo spawn near hospital after death
         }
