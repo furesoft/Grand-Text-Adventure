34ed609852856fefc2cfb0315d8150daf5b16fa3
Chris <filmee24@gmail.com>
2021-05-30 08:40:59 +0200

    Add Contact List to Phone

--- Source/GrandTextAdventure/GrandTextAdventure.Core/Phone.cs
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/Phone.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using LiteDB;
 
 namespace GrandTextAdventure.Core
 {
     public static class Phone
     {
         private static readonly Dictionary<string, Action> s_numberHandlers = new();
+        private static Dictionary<string, string> s_contacts = new();
 
         public static void Dial(string number)
         {
@@ -19,9 +21,44 @@ namespace GrandTextAdventure.Core
             }
         }
 
-        public static void AddNumber(string number, Action handler)
+        public static void AddNumberHandler(string number, Action handler)
         {
             s_numberHandlers.Add(number, handler);
+        }
+
+        public static void AddContact(string name, string number)
+        {
+            if (!s_contacts.ContainsKey(name))
+            {
+                s_contacts.Add(name, number);
+            }
+        }
+
+        public static IEnumerable<string> GetContactNames()
+        {
+            return s_contacts.Keys;
+        }
+
+        public static void Load()
+        {
+            using LiteDatabase db = new("gta.conf"); // ToDo: need to set config path
+            var contactsCollection = db.GetCollection<KeyValuePair<string, string>>("contacts");
+
+            s_contacts = new Dictionary<string, string>(contactsCollection.FindAll());
+        }
+
+        public static void Save()
+        {
+            using LiteDatabase db = new("gta.conf"); // ToDo: need to set config path
+            var contactsCollection = db.GetCollection<KeyValuePair<string, string>>("contacts");
+
+            foreach (var contact in s_contacts)
+            {
+                if (!contactsCollection.Exists(Query.EQ("Key", contact.Key)))
+                {
+                    contactsCollection.Insert(contact);
+                }
+            }
         }
     }
 }
