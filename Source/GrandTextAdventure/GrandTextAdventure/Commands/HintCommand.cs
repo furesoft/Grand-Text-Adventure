4110ab401addc91300999cae3c5ee2f7cedfba28
Chris <filmee24@gmail.com>
2021-05-30 11:21:42 +0200

    Add Hint Command

---
+++ Source/GrandTextAdventure/GrandTextAdventure/Commands/HintCommand.cs
@@ -0,0 +1,15 @@
+using GrandTextAdventure.Core.TextProcessing;
+using GrandTextAdventure.Core.TextProcessing.Interfaces;
+using GrandTextAdventure.Core.TextProcessing.Synonyms;
+
+namespace GrandTextAdventure.Commands
+{
+    [CommandHandler(VerbCodes.Hint)]
+    public class HintCommand : ICommandHandler
+    {
+        public void Invoke(Command cmd)
+        {
+            System.Console.WriteLine("There is no Help at the moment available");
+        }
+    }
+}
