c8d2bc68962aa0dd6c6771533e97139e29dccbfe
Chris <filmee24@gmail.com>
2021-05-30 08:47:05 +0200

    Add Loading Dialog from JSON

--- Source/GrandTextAdventure/GrandTextAdventure.Core/DialogItem.cs
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/DialogItem.cs
@@ -1,7 +1,17 @@
+using System.IO;
+
 namespace GrandTextAdventure.Core
 {
     public record DialogItem(string Name, string[] Lines, DialogItem Next, string[] ChooseLines = null)
     {
+        public static DialogItem FromJsonStream(Stream strm)
+        {
+            var reader = new StreamReader(strm);
+            var result = System.Text.Json.JsonSerializer.Deserialize<DialogItem>(reader.ReadToEnd());
 
+            reader.Close();
+
+            return result;
+        }
     }
 }
