183aa26449d9b44c7f52807dc5a2c51d98efb375
filmee24 <filmee24@gmail.com>
2022-10-16 15:13:53 +0200

    Add Scheme Interpreter

---
+++ Source/GrandTextAdventure/GrandTextAdventure.Core/Script.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Reflection;
+using Darlek.Core.RuntimeLibrary;
+
+namespace GrandTextAdventure.Core
+{
+    public class Script
+    {
+        public Darlek.Scheme.Interpreter interpreter = new();
+
+        public Script()
+        {
+            SchemeCliLoader.Apply(Assembly.GetEntryAssembly(), interpreter);
+        }
+
+        public string Source { get; set; }
+
+        public object Execute()
+        {
+            return interpreter.Evaluate(new StringReader(Source));
+        }
+    }
+}
